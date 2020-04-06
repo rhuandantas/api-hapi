0ccd2d9f510cdb3e5a82134d6e9f153696191fdf
Rhuan <rhuannixon@gmail.com>
2020-04-06 00:22:44 -0300

    add swagger

--- index.js
+++ index.js
@@ -2,16 +2,32 @@
 
 const Hapi = require('@hapi/hapi');
 const routes = require('./src/routes');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
 
 const init = async () => {
-
+    const swaggerOptions = {
+        info: {
+            title: 'Books API Documentation',
+            version: '0.0.1',
+        }
+    };
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
     });
 
     server.route(routes);
-
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,4 @@
+const joi = require('@hapi/joi');
 const userHandler = require("./../handler/user");
 
 module.exports = [
@@ -5,25 +6,59 @@ module.exports = [
         method: "POST",
         path: "/signin",
         config: {
+            tags: ['api'],
             handler: userHandler.signin,
+            validate: {
+                payload: joi.object({
+                    email: joi.string().email().required(),
+                    password: joi.string().required(),
+                })
+            }
         }
     },
 
     {
         method: "POST",
         path: "/signup",
-        handler: userHandler.signup
+        config: {
+            description: "cadastrar um usuário",
+            notes: "teste",
+            tags: ['api'],
+            handler: userHandler.signup,
+            validate: {
+                payload: joi.object({
+                    name: joi.string().required(),
+                    email: joi.string().email().required(),
+                    password: joi.string().required(),
+                })
+            }
+        }
     },
 
     {
         method: "GET",
         path: "/user/{id}",
-        handler: userHandler.search
+        config: {
+            description: "buscar um user pelo id",
+            notes: "teste",
+            tags: ['api'],
+            handler: userHandler.search,
+            validate: {
+                params: joi.object({
+                    id: joi.string().required(),
+                })
+            }
+        }
     },
 
     {
         method: "GET",
         path: "/user",
-        handler: userHandler.getAll
+        config: {
+            description: "buscar todos os uers",
+            notes: "teste",
+            tags: ['api'],
+            handler: userHandler.getAll,
+        }
     }
 ];
